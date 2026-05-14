b718785b2fd4ca198ca0eb1e74f9e73109c6f869
artbavial <pipleworld@gmail.com>
2026-05-14 22:27:21 +0300

    Дополнительные изменения по сайту. Добавлен диапазон цен. Добавлена страница Контакты. Улучшена работа сайта.

---
+++ Migrations/20260514183808_AddPriceToKopeksToPageCity.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcobayApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPriceToKopeksToPageCity : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PriceToKopeks",
+                table: "PageCities",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PriceToKopeks",
+                table: "PageCities");
+        }
+    }
+}
